8d8018a00678ff494c1b993422a5b55158283817
GofarP <gofarperdana83@gmail.com>
2026-04-04 12:46:58 +0700

    Fix colspan

--- resources/views/livewire/cabang/cabang-index.blade.php
+++ resources/views/livewire/cabang/cabang-index.blade.php
@@ -37,7 +37,7 @@
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="4" class="text-center" style="border: 1px solid; color: black;">Data Tidak
+                        <td colspan="5" class="text-center" style="border: 1px solid; color: black;">Data Tidak
                             Tersedia Atau Masih Kosong</td>
                     </tr>
                 @endforelse
